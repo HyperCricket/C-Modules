19433911d556afc999992ebb86fcf198711350a7
HyperCricket <136273282+HyperCricket@users.noreply.github.com>
2024-07-22 17:12:38 -0500

    Create module_1.cpp

---
+++ C++ Modules/Section 4/module_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+	int favnum;
+	std::cout << "Enter your favorite number between 1-100: ";
+
+	std::cin >> favnum;
+
+	std::cout << "Amazing, that's my favorite number too!" << std::endl;
+	return 0;
+}
