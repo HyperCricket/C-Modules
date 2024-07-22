a7456ba05a4d23289a3b94419eb84c8359dd5396
HyperCricket <136273282+HyperCricket@users.noreply.github.com>
2024-07-22 17:13:19 -0500

    Add files via upload

---
+++ C++ Modules/Section 4/Challenge.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+// This is a comment
+
+int main()
+{
+	int favnum;
+	std:: cout << "Enter your favorite number between 1 and 100: ";
+
+	std::cin >> favnum;
+
+	std::cout << "Amazing, that's my favorite number too!" << std::endl;
+	std::cout << "No seriously, " << favnum << " is my favorite number!" << std::endl;
+	return 0;
+
+	
+} 
